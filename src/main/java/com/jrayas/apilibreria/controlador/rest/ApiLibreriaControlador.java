518ebb31e136970a595431dc90b7273f2709c38c
JorgeRayas <jorgerayasaleman@gmail.com>
2024-05-14 20:14:56 -0600

    Se cambia cors a clase de configuracion

--- src/main/java/com/jrayas/apilibreria/controlador/rest/ApiLibreriaControlador.java
+++ src/main/java/com/jrayas/apilibreria/controlador/rest/ApiLibreriaControlador.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,7 +28,6 @@ import com.jrayas.apilibreria.servicios.EliminaLibroServicio;
 import com.jrayas.apilibreria.servicios.ObtenerCatalogosServicio;
 import com.jrayas.apilibreria.servicios.ObtenerLibrosServicio;
 
-@CrossOrigin(origins = "*")
 @RestController
 public class ApiLibreriaControlador {
 
@@ -50,7 +48,7 @@ public class ApiLibreriaControlador {
 		this.servEliminaLibro = servEliminaLibro;
 		this.servObtenerLibros = servObtenerLibros;
 	}
-
+	
 	@GetMapping("/catalogos")
 	public ResponseEntity<ObtenerCatalogosRespuesta> obtenerCatalogos() {
 		return new ResponseEntity<>(servObtenerCatalogos.obtenerCatalogos(), HttpStatus.OK);
